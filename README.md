4ad6d5d71ba9f3af77627052f4512c3fef79ce91
Alexandre Leroux <alex@sherpa.one>
2023-10-08 19:38:35 +0200

    add sources

---
+++ README.md
@@ -0,0 +1,30 @@
+# Rotten Pizzas Shop
+
+## 🚨🚨🚨 Attention 🚨🚨🚨
+
+Le code présent dans ce projet contient volontairement de nombreuses imperfections :
+
+- erreurs de conception,
+- mauvaises pratiques de développement logiciel,
+- failles de sécurité et de performance.
+
+Ce projet servira de support à un exercice de refactoring.
+
+Votre objectif est :
+
+- d'analyser le code source,
+- d'identifier les erreurs,
+- de corriger les erreurs avec la stack technologique initiale (ou celle de votre choix),
+- de mettre en place une stratégie permettant d'assurer la qualité du logiciel (tests, audit, intégration continue, absence de régression...).
+
+--
+
+!["Sherpa"](https://sherpa.one/images/sherpa-logotype-120.png)
+
+__Alexandre Leroux__  
+_Enseignant / Formateur_  
+_Développeur logiciel web & mobile_
+
+Nancy (Grand Est, France)
+
+<https://sherpa.one>
---
+++ api/routes/orders.js
@@ -0,0 +1,112 @@
+/**
+ * 🚨 Attention, le code présent dans ce fichier contient volontairement de nombreuses imperfections :
+ * 🚨 erreurs de conception, mauvaises pratiques de développement logiciel, failles de sécurité et de performance.
+ * 🚨 Ce code servira de support à un exercice de refactoring.
+ */
+
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "db",
+  user: "rps",
+  password: "azerty",
+  database: "rps"
+});
+
+connection.connect();
+
+router.use(express.json());
+
+//CREATE
+router.post("/", function (req, res) {
+  let token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
+    if (err) res.json("unauthorized");
+    connection.query(
+      `INSERT INTO orders (user_id, amount, createdAt) VALUES (${req.body.id}, ${req.body.amount}, NOW())`,
+      (err, rows, fields) => {
+        if (err) console.log(err);
+
+        const orderId = rows.insertId;
+
+        for (const pizzaId of req.body.pizzas) {
+          connection.query(
+            `INSERT INTO orders_pizzas (order_id, pizza_id, quantity) VALUES (${orderId},${pizzaId},1)`,
+            (err, rows, fields) => {
+              if (err) console.log(err);
+            }
+          );
+        }
+
+        res.json("created");
+      }
+    );
+  });
+});
+
+//READ ALL
+router.get("/", function (req, res) {
+  //FILTER BY USER ID
+  if (req.query.user_id) {
+    const query = `SELECT * FROM orders WHERE user_id = "${req.query.user_id}"`;
+
+    connection.query(query, (err, rows, fields) => {
+      if (err) console.log(err);
+
+      res.json(rows);
+    });
+  } else if (req.query.status) {
+    const query = `SELECT * FROM orders WHERE status = ${req.query.status}`;
+
+    connection.query(query, (err, rows, fields) => {
+      if (err) console.log(err);
+
+      res.json(rows);
+    });
+  } else {
+    connection.query("SELECT * FROM orders", (err, rows, fields) => {
+      if (err) console.log(err);
+
+      res.json(rows);
+    });
+  }
+});
+
+//UPDATE ONE BY ID
+router.put("/:id", function (req, res) {
+  let token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
+    if (err) res.json("unauthorized");
+
+    const query = `UPDATE orders SET id = ${req.body.id}, status = "${req.body.status}", amount = ${req.body.amount} WHERE id = ${req.body.id}`;
+
+    connection.query(query, (err, rows, fields) => {
+      if (err) console.log(err);
+
+      res.json("updated");
+    });
+  });
+});
+
+//READ ONE BY ID
+router.get("/:id", function (req, res) {
+  const query = `SELECT * FROM orders WHERE id =${req.params.id}`;
+
+  connection.query(query, (err, rows, fields) => {
+    if (err) console.log(err);
+
+    res.json(rows);
+  });
+});
+
+module.exports = router;
+/**
+ * 🚨 Attention, le code présent dans ce fichier contient volontairement de nombreuses imperfections :
+ * 🚨 erreurs de conception, mauvaises pratiques de développement logiciel, failles de sécurité et de performance.
+ * 🚨 Ce code servira de support à un exercice de refactoring.
+ */
